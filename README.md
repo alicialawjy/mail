c9a3bfb7011a88ea704ee3b53681be1d25029cb5
alicialawjy <alicialawjy@gmail.com>
2021-04-26 23:01:21 +0100

    Fixed Mark As Unread/Archive button bug in inbox.js

--- README.md
+++ README.md
@@ -3,4 +3,47 @@
 A Gmail-like email platform that makes API calls to send and receive emails. <br>
 This project was done as part of the Harvard x EdX Course: CS50 Web Programming with Python and JavaScript.
 
+## Project Setup
+1. Download the zip file to this repository.
+2. In your terminal, cd into the 'mail' directory.
+3. Run ```python manage.py runserver``` and visit <a>http://127.0.0.1:8000/</a>
+
+## Tech Stack
+- Backend Framework: Django and Python
+- Database Modelling: SQLite
+- Front End Languages: HTML, CSS and Javascript
+
+## Functionality
+All functionalities were done by calling the application's API (written in Python) using JavaScript.
+#### 1. Send Mail
+- Done by the 'Compose' button in the sidepane. 
+- When a user submits the email composition form, a POST request is made to '/emails' (views.py: name=compose) to process the form into the database.
+
+#### 2. View Mailbox: Inbox/ Sent/ Archived
+- Done by clicking the mailbox in the sidepane. The active mailbox will be highlighted in red in the sidepane.
+- A GET request is sent to '/emails/<str:mailbox>' (views.py: name=mailbox) to request the email for the relevant mailbox.
+- If a mail has been read, the email will be shown with a gray background. Else, it will appear white. 
+
+#### 3. View Mail
+- Done by clicking on the email in the mailbox.
+- A GET request is sent to '/emails/<int:email_id>' (views.py: name=email) to request the email.
+- Shows the individual view and all contents of the email.
+
+#### 4. Archive and Unarchive Mail
+- Done via the archive/unarchive button in the individual email view. Only for inbox/ archived mail.
+- Once marked as archived, the email will move from the inbox to the archive mailbox (and vice versa)
+- A PUT request is sent to '/emails/<int:email_id>' (views.py: name=email).
+
+#### 5. Mark as Unread
+- If a post has been read, the post can be 'marked as unread' via the button in the individual email view. Only for inbox/ archived mail.
+- In the mailbox view, the email div will then have a white background instead of grey.
+- A PUT request is sent to '/emails/<int:email_id>' (views.py: name=email).
+
+#### 6. Reply an Email
+- Done via the reply button in the individual email view.
+- Users will be brought to the email composition form, where fields are prefilled: 
+    - Recipient: set to whoever sent the original email,
+    - Subject: Start with 'Re:' followed by the original email's subject line
+    - Body: prefilled with the original email's body.
+
 
--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -35,16 +35,6 @@ function compose_email() {
 //-------------- FUNCTION: GET 'INBOX/SENT/ARCHIVED' VIEW --------------//
 function load_mailbox(mailbox) {
 
-  // Send a GET request to views.py: name=mailbox & load all the mailbox content
-  fetch(`/emails/${mailbox}`)
-  .then(response => response.json())
-  .then(emails => {
-    // Print emails in console
-    console.log(emails);
-    // Show each post as a separate row
-    emails.forEach(add_post);
-  })
-
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -52,6 +42,15 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Send a GET request to views.py: name=mailbox & load all the mailbox content
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails in console
+    console.log(emails);
+    emails.forEach(add_post);
+  })
 
   // Highlight the button in the sidebar
   highlight_button(mailbox);
@@ -67,6 +66,7 @@ function highlight_button(mailbox) {
     button.style.border = 'none';
     button.style.color = 'gray';
   });
+
   // Highlight the active button belonging to the current active view
   const button = document.querySelector(`#${mailbox}`);
   button.style.backgroundColor = 'rgb(255, 196, 196)';
@@ -85,7 +85,7 @@ function add_post(contents) {
   // Style the div based on its 'Read' status
   mail.style.border = '1px solid rgb(26, 95, 243)';
   if (contents.read === true) {
-    mail.style.backgroundColor = 'rgb(235, 234, 234)';
+    mail.style.backgroundColor = 'rgb(241, 241, 241)';
     mail.style.border = '1px solid rgb(148, 148, 148)';
   }
 
@@ -95,79 +95,230 @@ function add_post(contents) {
   if (mailbox === 'Sent') {
     mail.innerHTML = `
       <div class='to-from-email'> ${contents.recipients} </div>
-      <div class='email-content'> ${contents.subject} </div>
+      <div class='email-content'> ${contents.subject} - ${contents.body} </div>
       <div class='email-timestamp'> ${contents.timestamp} </div>`;
   }
   // Else, show sender emails 
   else {
     mail.innerHTML = `
     <div class='to-from-email'> ${contents.sender} </div>
-    <div class='email-content'> ${contents.subject} </div>
+    <div class='email-content'> ${contents.subject} - ${contents.body} </div>
     <div class='email-timestamp'> ${contents.timestamp} </div>`;
   }
   
   // Append the div with the other emails
   document.querySelector('#emails-view').append(mail);
+
+  // Add an click event listener to each div
+  mail.addEventListener("click", () => indiv_mail(mail.id,mailbox));
+}
+
+//------------ FUNCTION: SHOW INDIVIDUAL MAIL VIEW -------------//
+function indiv_mail(mail_id, mailbox) {
+
+  // Show individual view for that email and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#indiv-view').style.display = 'block';
+
+  // Get the mail info,
+  fetch(`/emails/${mail_id}`)
+  .then(response => response.json())
+  .then(contents => {
+    document.querySelector('#indiv-info').innerHTML = `
+      From: ${contents.sender} <br>
+      To: ${contents.recipients} <br>
+      Subject: ${contents.subject} <br>
+      Timestamp: ${contents.timestamp}`;
+    document.querySelector('#indiv-body').innerHTML = `${contents.body}`;
+
+    // Asign data to the archived, read and reply buttons
+    document.querySelector('#archived_status').dataset.mail_id = mail_id;
+    document.querySelector('#read_status').dataset.mail_id = mail_id;
+    document.querySelector('#reply').dataset.mail_id = mail_id;
+
+    // Settle the button visibility
+    // Switch of Archive/ Mark as Unread button if it's a Sent mail.
+    if (mailbox === 'Sent') {
+      document.querySelector('#archived_status').style.display = 'none';
+      document.querySelector('#read_status').style.display = 'none';
+    }
+    else {
+      // Settle the Archive/ Read button inner HTML according to read/archived status
+      // Archived
+      document.querySelector('#archived_status').style.display = 'inline';
+      if (contents.archived === true) {
+        document.querySelector('#archived_status').innerHTML = 'Unarchive';
+      }
+      if (contents.archived === false) {
+        document.querySelector('#archived_status').innerHTML = 'Archive';
+      }
+    
+      // Read
+      if (contents.read === true) {
+        document.querySelector('#read_status').innerHTML = 'Mark As Unread';
+        document.querySelector('#read_status').style.display = 'inline';
+      }
+      if (contents.read === false) {
+        document.querySelector('#read_status').innerHTML = 'Read';
+        document.querySelector('#read_status').style.display = 'none';
+      }
+    }
+  })
+
+  // Mark email as read: send PUT request
+  fetch(`/emails/${mail_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: true
+    })
+  })
 }
 
 ////////////////////////////////////////////////////////////////////
-//                DIRECT TO INDIVIDUAL EMAIL VIEW                 //
+//                       COMPOSE NEW EMAIL                        //
 ////////////////////////////////////////////////////////////////////
 document.addEventListener('DOMContentLoaded', function() {
-  // Look at all email divs
-  document.querySelectorAll('.email').forEach(email => {
-    // When a div is clicked
-    email.addEventListener('click', function() { 
-      // Show individual view for that email and hide other views
-      document.querySelector('#emails-view').style.display = 'none';
-      document.querySelector('#compose-view').style.display = 'none';
-      document.querySelector('#indiv-view').style.display = 'block';
-
-      // Get the mail info,
-      const mail_id = email.id;
-      fetch(`/emails/${mail_id}`)
+  // When form is submitted,
+  document.querySelector("#compose-form").addEventListener('submit', function() {
+      // Send the form to views.py: name=email for processing
+      fetch('/emails', {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value
+      })
+      })
       .then(response => response.json())
-      .then(contents => {
-        document.querySelector('#indiv-view').id = mail_id;
-        document.querySelector('#indiv-info').innerHTML = `
-          From: ${contents.sender} <br>
-          To: ${contents.recipients} <br>
-          Subject: ${contents.subject} <br>
-          Timestamp: ${contents.timestamp}`;
-        document.querySelector('#indiv-body').innerHTML = `${contents.body}`;
+      .then(result => {
+          if (result.error) {
+              console.log(`Error sending email: ${result.error}`);
+          } 
+          else {
+              console.log(`Message:${result.message}`);
+          }
+      })
 
-        //Settle the Archived button 
-        if (contents.archived === true) {
-          document.querySelector('#archived_status').innerHTML = 'Unarchive';
-        }
-        else {
-          document.querySelector('#archived_status').innerHTML = 'Archive';
-        }
-
-        //Settle the Read button 
-        if (contents.read === true) {
-          document.querySelector('#read_status').innerHTML = 'Mark As Unread';
-          document.querySelector('#read_status').style.display = 'inline';
-        }
-        else {
-          document.querySelector('#read_status').innerHTML = 'Read';
-          document.querySelector('#read_status').style.display = 'none';
-        }
-
-        // Asign data to the archived, read and reply buttons
-        document.querySelector('#archived_status').dataset.mail_id = `${contents.id}`;
-        document.querySelector('#read_status').dataset.mail_id = `${contents.id}`;
-        document.querySelector('#reply').dataset.mail_id = `${contents.id}`;
-      });
-      // Mark email as read: send PUT request
-      fetch(`/emails/${mail_id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          read: true
-        })
-      });
-    });
+      // Stop form from submitting
+      return false
   });
 });
 
+////////////////////////////////////////////////////////////////////
+//           'ARCHIVE' AND 'MARK AS UNREAD' BUTTON                //
+////////////////////////////////////////////////////////////////////
+document.addEventListener('DOMContentLoaded', function() { 
+  // When the buttons are clicked, call the respective functions
+  document.querySelector('#archived_status').addEventListener('click', archive_button);
+  document.querySelector('#read_status').addEventListener('click', read_button);
+}); 
 
+//------------ FUNCTION: EXECUTE THE ARCHIVE BUTTON -------------//
+function archive_button() {
+
+  const archive = document.querySelector('#archived_status');
+  mail_id = archive.dataset.mail_id;
+  console.log(mail_id);
+
+  // If button call to 'Unarchive',
+  if (archive.innerHTML === 'Unarchive') {
+
+    // Send a PUT request to change archive status to false
+    fetch(`/emails/${mail_id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          archived: false
+      })
+    })
+    
+    // Send user to inbox where the email should now be visible (aka no longer archived)
+    load_mailbox('inbox');
+
+    // Change button to Archive
+    archive.innerHTML = 'Archive';
+  }
+
+  else {
+
+    // Send PUT request to archive the mail
+    fetch(`/emails/${mail_id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        archived: true
+      })
+    })
+
+    // Send user to archive mailbox where the email should now be visible (aka archived)
+    load_mailbox('archived');
+
+    // Change button to show 'Unarchive'
+    archive.innerHTML = 'Unarchive';
+  }
+}
+
+//----------- FUNCTION: EXECUTE THE 'MARK AS UNREAD' BUTTON -------------//
+function read_button() {
+  const read = document.querySelector('#read_status');
+  mail_id = read.dataset.mail_id;
+
+  // If call to mark email as unread:
+  if (read.innerHTML === 'Mark As Unread') {
+
+    // Send PUT request to mark mail status as unread
+    fetch(`/emails/${mail_id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          read: false
+      })
+    })
+
+    // Change text in button to 'Read' and hide the button
+    read.innerHTML = 'Read';
+    read.style.display = 'none';
+
+    // Direct user to view based on archive status
+    // If it's an archived email, show the archive mailbox
+    if (document.querySelector('#archived_status').innerHTML === 'Unarchive'){
+      load_mailbox('archived');
+    }
+    // Else, it's in the inbox, therefore show inbox
+    else {
+      load_mailbox('inbox');
+    }
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////
+//                     REPLY TO AN EMAIL                          //
+////////////////////////////////////////////////////////////////////
+document.addEventListener('DOMContentLoaded', function() { 
+  // When click on reply button,
+  document.querySelector('#reply').addEventListener('click', function() {
+    // Show the 'compose email' form
+    compose_email();
+    // Get info of the email we are replying to so we can prefill the text area with this
+    mail_id = document.querySelector('#reply').dataset.mail_id;
+    fetch(`/emails/${mail_id}`)
+    .then(response => response.json())
+    .then(email => {
+      // Print emails
+      console.log(email);
+      //Prefill recipients
+      document.querySelector('#compose-recipients').value = email.sender;
+      //Prefill subject. Insert Re: if doesn't already contain it
+      if (email.subject.slice(0,3)==='Re:'){
+        document.querySelector('#compose-subject').value = email.subject;
+      }
+      else{
+        document.querySelector('#compose-subject').value = `Re: ${email.subject}`;
+      }
+      //Prefill body
+      document.querySelector('#compose-body').value = `\n On ${email.timestamp} ${email.sender} wrote:" ${email.body}`;
+    })
+  });
+});
